589a2e5f8571a33c48cc10e05ee6410840669fde
tonik7599 <tanygin7599@gmail.com>
2018-01-16 16:53:59 +0300

    second commit

--- a.cpp
+++ a.cpp
@@ -1,61 +1,60 @@
 #include <iostream>
 
+template<typename T>
 struct MyList {
-    int value;
-    MyList * p;
+    T value;
+    MyList<T> * next;
 
-    MyList()
-            : p(nullptr)
+    MyList<T>()
+            : next(nullptr)
     { }
 
-    MyList(int a)
+    MyList<T>(T a)
             : value(a)
-            , p(nullptr)
+            , next(nullptr)
     { }
+
+    ~MyList<T>() {
+        delete *this;
+    }
 };
 
-MyList* merge(MyList* first, int n) {
+template<typename T>
+MyList<T>* merge(MyList<T>* first, int n) {
     if(n == 1)
         return first;
 
-    MyList* second = first;
-    int length1 = n / 2;
-    int length2 = n - n / 2;
-    for (int i = 0; i != length1 - 1; ++i) {
-        second = second->p;
+    MyList<T>* second = first;
+    for (int i = 0; i != n / 2 - 1; ++i) {
+        second = second->next;
     }
-    MyList* term = second;
-    second = second->p;
-    term->p = nullptr;
-    first = merge(first, length1);
-    second = merge(second, length2);
-    MyList* num;
-    MyList* last;
-    if (first->value <= second->value) {
-        num = first;
-        last = num;
-        first = first->p;
-    } else {
-        num = second;
-        last = num;
-        second = second->p;
-    }
+    MyList<T>* term = second;
+    second = second->next;
+    term->next = nullptr;
+    first = merge(first, n / 2);
+    second = merge(second, n - n / 2);
+    MyList<T>* num = nullptr;
+    MyList<T>* last = nullptr;
 
-    while (first != nullptr && second != nullptr) {
-        if (first->value <= second->value) {
-            last->p = first;
+    while (first != nullptr || second != nullptr) {
+        if (!second || (first && first->value < second->value)) {
+            if (last) {
+                last->next = first;
+            } else {
+                num = first;
+            }
             last = first;
-            first = first->p;
+            first = first->next;
         } else {
-            last->p = second;
+            if (last) {
+                last->next = second;
+            } else {
+                num = second;
+            }
             last = second;
-            second = second->p;
+            second = second->next;
         }
     }
-    if (first == nullptr)
-        last->p = second;
-    if (second == nullptr)
-        last->p = first;
 
     return num;
 }
@@ -63,24 +62,25 @@ MyList* merge(MyList* first, int n) {
 int main() {
     int n;
     std::cin >> n;
-    MyList* last = new MyList;
-    MyList* begin = last;
-    for (int i = 0; i != n; ++i) {
+    MyList<int>* last = new MyList<int>;
+    MyList<int>* begin = last;
+    for (int i = 0; i < n; ++i) {
         int a;
         std::cin >> a;
         last->value = a;
         if (i != n - 1) {
-            last->p = new MyList;
-            last = last->p;
+            last->next = new MyList<int>;
+            last = last->next;
         } else {
-            last->p = nullptr;
+            last->next = nullptr;
         }
     }
     last = merge(begin, n);
 
-    MyList* iter = last;
+    MyList<int>* iter = last;
     while (iter != nullptr) {
         std::cout << iter->value << ' ';
-        iter = iter->p;
+        iter = iter->next;
     }
+    return 0;
 }
